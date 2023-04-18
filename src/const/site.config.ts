7eef221ed2feb8d23441eaedd68fe2f117a0273e
SatakeYusuke19920527 <satkyusk@icloud.com>
2023-04-18 22:16:38 +0900

    add : layouter

---
+++ src/const/site.config.ts
@@ -0,0 +1,6 @@
+export const siteConfig = {
+  title: 'between',
+  twitterUrl: 'https://github.com/Hattori-Takuma/between-web',
+  InstagramUrl: 'https://github.com/Hattori-Takuma/between-web',
+  githubUrl: 'https://github.com/Hattori-Takuma/between-web',
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,19 @@
-import { Inter } from 'next/font/google'
+import Layout from '@/components/Layout';
+import { Inter } from 'next/font/google';
 
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <section className="h-screen w-4/5 max-w-5xl mx-auto flex items-center justify-
-center flex-col">
-<h1 className="mb-4 text-green-500 text-3xl">サンプル</h1>
-<p className="mb-2 text-center">sample text</p>
-<button className="btn-blue">Lets Start!!</button>
-</section>
-  )
+    <Layout>
+      <section
+        className="h-screen w-4/5 max-w-5xl mx-auto flex items-center justify-
+center flex-col"
+      >
+        <h1 className="mb-4 text-green-500 text-3xl">サンプル</h1>
+        <p className="mb-2 text-center">sample text</p>
+        <button className="btn-blue">Lets Start!!</button>
+      </section>
+    </Layout>
+  );
 }
