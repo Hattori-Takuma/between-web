093f6d7a813cbd4ab1de7d2e70ddd6c73610dd6d
SatakeYusuke19920527 <satkyusk@icloud.com>
2023-06-03 14:51:22 +0900

    ログインユーザのfirestore内で単一化

--- src/models/userInfoApplicationService.ts
+++ src/models/userInfoApplicationService.ts
@@ -1,13 +1,11 @@
-import { addDoc, collection, getFirestore, serverTimestamp } from "firebase/firestore";
-
+import { doc, getFirestore, setDoc } from 'firebase/firestore';
 
 export const db = getFirestore();
 
 export const userInfo = async (user: any) => {
-  const docRef = await addDoc(collection(db, "users"), {
+  await setDoc(doc(db, 'users', user.uid), {
     name: user.displayName,
     uid: user.uid,
-    address: "test"
+    address: 'test',
   });
-  console.log("Document written with ID: ", docRef.id);
-}
+};
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,49 +1,33 @@
 import Layout from '@/components/Layout';
-import Link from 'next/link'
-import { Inter } from 'next/font/google';
-import Router from 'next/router'
-import { googleLogin } from "../models/authApplicationServics"
 import { userInfo } from '@/models/userInfoApplicationService';
-
+import { Inter } from 'next/font/google';
+import Link from 'next/link';
+import Router from 'next/router';
+import { googleLogin } from '../models/authApplicationServics';
 
 const inter = Inter({ subsets: ['latin'] });
 
-const handler = (path:string) => {
-  Router.push(`${path}/`)
-}
+const handler = (path: string) => {
+  Router.push(`${path}/`);
+};
 
 const google = async () => {
-  const user = await googleLogin()
-  await userInfo(user)
-  handler('/friends')
-console.log('user',user)
-}
-  
+  const user = await googleLogin();
+  console.log('🚀 ~ file: login.tsx:17 ~ google ~ user:', user);
+  await userInfo(user);
+  handler('/friends');
+  console.log('user', user);
+};
 
 const login = () => {
   return (
     <Layout>
-      
-        <Link href="/">
-          [戻る]
-      </Link>
-      
-      
-    
+      <Link href="/">[戻る]</Link>
 
-         <Link href="/friends">
-          [ログイン]
-      </Link>
-      
-      
+      <Link href="/friends">[ログイン]</Link>
 
-         <button onClick={google}>
-        [googleログイン]
-      </button>
-
-      
-   
+      <button onClick={google}>[googleログイン]</button>
     </Layout>
-  )
-}
-  export default login;
+  );
+};
+export default login;
