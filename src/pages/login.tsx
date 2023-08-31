a453ae1d70f1a7feb17827e1658f16967a3b08e6
SatakeYusuke19920527 <satkyusk@icloud.com>
2023-08-31 22:28:13 +0900

    commit

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -21,7 +21,7 @@ const Login = () => {
       setLocationFunc(user);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [user, location]);
+  }, [location]);
   // login
   const google = async () => {
     const user = await googleLogin();
