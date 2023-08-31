bc7ca404904c529e00a41d0709fa85f2b5e22f76
Hattori-Takuma <hattori.ta@icloud.com>
2023-08-31 21:45:01 +0900

    map

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
-import Map from '../models/getMap';
+// import Map from '../models/getMap';
 import Link from 'next/link';
 
 function Maps() {
   return (
     <div className="App">
-      <Map />
+      map
+      {/* <Map /> */}
 
       <Link href="/friendsSarch">戻る←</Link>
     </div>
