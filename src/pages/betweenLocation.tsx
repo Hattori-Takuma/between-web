9fbb5386356c64d920462d7592ab3839e4e50061
Hattori-Takuma <hattori.ta@icloud.com>
2023-09-02 14:28:33 +0900

    error解消中

--- src/pages/betweenLocation.tsx
+++ src/pages/betweenLocation.tsx
@@ -1,20 +1,19 @@
 import Layout from '@/components/Layout';
-import { useLocation } from '../hooks/useLocation';
+// import { useLocation } from '../hooks/useLocation';
 import Link from 'next/link';
 import Router from 'next/router';
 
 
 const betweenLocation = () => {
-  const { latitude, longitude, location } = useLocation();
+  // const { latitude, longitude, location } = useLocation();
   
-  const { latitude: latitude1, longitude: longitude1 } = useLocation();
-  const { latitude: latitude2, longitude: longitude2 } = useLocation();
+  // const { latitude: latitude1, longitude: longitude1 } = useLocation();
+  // const { latitude: latitude2, longitude: longitude2 } = useLocation();
 
-  // 中間地点の緯度と経度を計算
-  const middleLatitude = (latitude + latitude2) / 2;
-  const middleLongitude = (longitude + longitude2) / 2;
+  // // 中間地点の緯度と経度を計算
+  // const middleLatitude = (latitude + latitude2) / 2;
+  // const middleLongitude = (longitude + longitude2) / 2;
   
- 
 
     return (
       <div>
@@ -23,9 +22,9 @@ const betweenLocation = () => {
       <h1>中間地点計算</h1>
       <div>
         <h2>位置1:</h2>
-        <p>緯度: {latitude}</p>
+        {/* <p>緯度: {latitude}</p>
           <p>経度: {longitude}</p>
-          <p>経度: {location}</p>
+          <p>経度: {location}</p> */}
       </div>
       {/* <div>
         <h2>位置2:</h2>
@@ -39,6 +38,7 @@ const betweenLocation = () => {
           <p>経度: {middleLongitude}</p>
         </div>
         )} */}
+         <Link　href="/friendsSarch">戻る</Link>
         
 
     </div>
